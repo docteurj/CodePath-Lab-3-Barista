eb5fe480b8b7c93356cc64f2070cf87be50feca4
docteurj <jonathandocteur@gmail.com>
2023-03-07 20:35:00 -0500

    Update App.css+jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { useState } from "react";
+import "./App.css";
+import BaristaForm from "./components/baristaForm";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div className="title-container">
+        <h1 className="title">On My Grind</h1>
+        <p>So you think you can barista? Let's put that to the test...</p>
+      </div>
+      <BaristaForm />
+    </div>
+  );
+}
+
+export default App;
